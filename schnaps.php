8aae165b17ce0f38dd1d2e595276485360e829dc
Guy Halford-Thompson <mrwooster@gmail.com>
2010-12-28 20:50:33 +0000

    Some fixes.  Locking of mysql tables and freezing of filesystem now functional

--- schnaps.php
+++ schnaps.php
@@ -1,20 +1,20 @@
 <?php
 /*
- * Schnapps backup script
+ * Schnaps backup script
  *
  * Automatic snapshot management for Amazon EC2
  */
 
 // Initial configuration
 $path = dirname(__FILE__);
-$config_file = 'schnapps.ini';
+$config_file = 'schnaps.ini';
 
 // Load logger
 require('logger.php');
-$logger = new GhtLib/Logger('schnapps.log');
+$logger = new GhtLib\Logger('schnaps.log');
 $logger->init();
 
-$logger->log('Schnapps started');
+$logger->log('Schnaps started');
 
 // Load configs
 $config = parse_ini_file($config_file);
@@ -23,27 +23,25 @@ $config = parse_ini_file($config_file);
 if ($config['lock_mysql_db'] == 'true') {
 	if (!$db = mysql_pconnect($config['db_server'], $config['db_username'], $config['db_password'])) {
 		$logger->log('Error connecting to mysql: '.mysql_error($db), 'e');
-		die(mysql_error($db);
+		die(mysql_error($db));
 	}
 	$logger->log('Connected to mysql db');
 }
 
-	// Flush tables
+// Flush tables
 if ($config['lock_mysql_db'] == 'true') {
 	if(!mysql_query('FLUSH TABLES WITH READ LOCK')) {
 		$logger->log('Error locking tables: '.$mysql_error($db), 'e');
-		die(mysql_error($db);
+		die(mysql_error($db));
 	}
 	$logger->log('Obtained lock on mysql db');
 }
 
 // Freeze filesystem
 if ($config['lock_xfs_filesystem'] == 'true') {
-	exec('xfs_freeze -f '.$config['xfs_mountpoint'];
-	$logger->log('Filesystem frozen');
+	exec('xfs_freeze -f '.$config['xfs_mountpoint']);
 }
 // ... Initiate snapshot
-$logger->log('This is where the snapshot would be done');
 
 // Unfreeze filesystem
 if ($config['lock_xfs_filesystem'] == 'true') {
@@ -55,9 +53,9 @@ if ($config['lock_xfs_filesystem'] == 'true') {
 if ($config['lock_mysql_db'] == 'true') {
 	if (!mysql_query('UNLOCK TABLES')) {
 		$logger->log('Error unlocking tables: '.$mysql_error($db), 'e');
-		die(mysql_error($db);
+		die(mysql_error($db));
 	}
 	$logger->log('Unlocked mysql db');
 }
 
-$logger->log('Schnapps finished');
+$logger->log('Schnaps finished');
